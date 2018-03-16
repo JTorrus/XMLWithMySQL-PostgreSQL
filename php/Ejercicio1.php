7ead8ebfa152f9900440da68661ad0cfa060b24f
JTorrus <jtorrusgalindo@gmail.com>
2018-03-16 02:04:12 +0100

    Inserting from XML to MySQL database

--- php/Ejercicio1.php
+++ php/Ejercicio1.php
@@ -1,2 +1,19 @@
 <?php
 
+$xml = simplexml_load_file("../xml/menu.xml");
+
+$conn = mysqli_connect("localhost", "root");
+mysqli_select_db($conn,"ejercicio1a");
+
+foreach ($xml->menjar as $row) {
+    $name = $row->nom;
+    $price = $row->preu;
+    $category = $row->categoria;
+    $description = $row->descripcio;
+    $calories = $row->calories;
+
+    $sql = "INSERT INTO `xmlinfo` (`text_field`)
+            VALUES('$name // $price // $category // $description // $calories')";
+
+    $result = mysqli_query($conn, $sql);
+}
